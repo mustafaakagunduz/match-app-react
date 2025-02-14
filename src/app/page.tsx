ac449d632c7eff580391926130d0bcd8616fda06
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-02-14 20:50:27 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ const JobAnalyzer = () => {
     setLoading(true);
     setError('');
     try {
-      const apiKey = process.env.API_KEY;
+      const apiKey = process.env.API_KEY
 
       if (!apiKey) {
         throw new Error("API anahtarı bulunamadı");
